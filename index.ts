a8d02555cdb54aab39edf528a3d9a3b35ef91225
ttngocthao <ttngocthao_87@yahoo.com>
2021-04-13 09:23:36 +0100

    initial typscript backend set up

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/hello',(_req,res)=>{
+    res.json('Hello pht typescript app');
+});
+
+const PORT: string|number = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server is running @ ${PORT}`);
+});
